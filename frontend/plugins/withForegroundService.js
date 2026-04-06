b7a5ee02024029077c5eae21f5154773b65283bf
Rohan.P <87313706+Rohan03march@users.noreply.github.com>
2026-04-06 18:27:14 +0530

    commit

--- frontend/plugins/withForegroundService.js
+++ frontend/plugins/withForegroundService.js
@@ -3,6 +3,7 @@ const { withAndroidManifest } = require("@expo/config-plugins");
 module.exports = function withForegroundService(config) {
   return withAndroidManifest(config, (config) => {
     const mainManifest = config.modResults.manifest;
+    mainManifest.$["xmlns:tools"] = "http://schemas.android.com/tools";
     const application = mainManifest.application[0];
 
     if (!application.service) {
@@ -20,11 +21,13 @@ module.exports = function withForegroundService(config) {
           "android:enabled": "true",
           "android:exported": "false",
           "android:foregroundServiceType": "camera|microphone|phoneCall|specialUse",
+          "tools:replace": "android:foregroundServiceType",
         },
       };
       application.service.push(service);
     } else {
       service.$["android:foregroundServiceType"] = "camera|microphone|phoneCall|specialUse";
+      service.$["tools:replace"] = "android:foregroundServiceType";
     }
 
     return config;
